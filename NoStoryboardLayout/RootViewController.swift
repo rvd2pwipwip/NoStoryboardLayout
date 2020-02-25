aac2a4d24d2c39067d20570f261addbe49a9f919
Herve Desrosiers <hdesrosiers@stingray.com>
2020-02-25 10:08:46 -0500

    added xib file for layout without storyboard

--- NoStoryboardLayout/RootViewController.swift
+++ NoStoryboardLayout/RootViewController.swift
@@ -9,11 +9,27 @@
 import UIKit
 
 class RootViewController: UIViewController {
-
+    
+    let padding: CGFloat = 50.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .yellow
     }
-
-
+    
+    override func viewWillLayoutSubviews() {
+        if greenView.superview == nil {
+            view.addSubview(greenView)
+            let width = view.bounds.width - 2 * padding
+            greenView.frame = CGRect(x: padding, y: padding, width: width, height: 3 * padding)
+        }
+    }
+    
+    private let greenView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .green
+        view.autoresizingMask = [.flexibleWidth, .flexibleBottomMargin]
+        return view
+    }()
+    
 }
